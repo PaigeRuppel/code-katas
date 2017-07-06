2afafa4a22cb812611078a3f8db09dd917e40c6f
Paige Ruppel <paige.ruppel@gmail.com>
2017-07-06 15:41:42 -0400

    wrote tests for 2-4 and passed

--- src/com/paigeruppel/romannumeralkata/v2/RomanToArabicConverter.java
+++ src/com/paigeruppel/romannumeralkata/v2/RomanToArabicConverter.java
@@ -2,10 +2,19 @@ package com.paigeruppel.romannumeralkata.v2;
 
 public class RomanToArabicConverter {
 
-	
 	public int convertToArabic(String input) {
-		int answer = 1;
-		
+		int answer = 0;
+
+		if (input.equals("I")) {
+			answer = 1;
+		} else if (input.equals("II")) {
+			answer = 2;
+		} else if (input.equals("III")) {
+			answer=3;
+		} else if (input.equals("IV")) {
+			answer=4;
+		}
+
 		return answer;
 	}
 }
--- src/com/paigeruppel/romannumeralkata/v2/RomanToArabicConverterTest.java
+++ src/com/paigeruppel/romannumeralkata/v2/RomanToArabicConverterTest.java
@@ -11,4 +11,26 @@ public class RomanToArabicConverterTest {
 		int response = underTest.convertToArabic("I");
 		Assert.assertEquals(1, response);
 	}
+	
+	@Test
+	public void shouldReturn2() {
+		RomanToArabicConverter underTest = new RomanToArabicConverter();
+		int response = underTest.convertToArabic("II");
+		Assert.assertEquals(2, response);
+	}
+	
+	@Test
+	public void shouldReturn3() {
+		RomanToArabicConverter underTest = new RomanToArabicConverter();
+		int response = underTest.convertToArabic("III");
+		Assert.assertEquals(3, response);
+	}
+	
+	@Test
+	public void shouldReturn4() {
+		RomanToArabicConverter underTest = new RomanToArabicConverter();
+		int response = underTest.convertToArabic("IV");
+		Assert.assertEquals(4, response);
+	}
+	
 }
